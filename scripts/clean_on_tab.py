19c5d590f63717014ac48f3122d4f598c7b7a330
wangcongling <wangcongling06883@hellobike.com>
2023-06-30 18:52:52 +0800

    update

--- scripts/clean_on_tab.py
+++ scripts/clean_on_tab.py
@@ -2,6 +2,7 @@ import modules.scripts as scripts
 import gradio as gr
 from pathlib import Path
 import os
+from datetime import datetime
 
 from modules import script_callbacks
 
@@ -17,22 +18,31 @@ def clean_image_to_images():
 
 def base_clean_action(category):
     global process_logs
-    
+    del_cnt = 0
+    now = datetime.now()
+
     if category == 1:
-        process_logs = process_logs + "done clean text to image...\n"
-        delete_image('outputs/txt2img-images')
-        delete_image('outputs/txt2img-grids')
+        del_cnt = del_cnt + delete_image('outputs/txt2img-images')
+        del_cnt = del_cnt + delete_image('outputs/txt2img-grids')
+        process_logs = process_logs + now.strftime("%Y-%m-%d %H:%M:%S") + "-> done clean text to image..." + "total delete:{}\n".format(del_cnt)
+    elif category == 2:
+        del_cnt = del_cnt + delete_image('outputs/img2img-images')
+        del_cnt = del_cnt + delete_image('outputs/img2img-grids')
+        process_logs = process_logs + now.strftime("%Y-%m-%d %H:%M:%S") + "-> done clean image to image..." + "total delete:{}\n".format(del_cnt)
+    elif category == 3:
+        del_cnt = del_cnt + delete_image('outputs/txt2img-images')
+        del_cnt = del_cnt + delete_image('outputs/txt2img-grids')
+        del_cnt = del_cnt + delete_image('outputs/img2img-images')
+        del_cnt = del_cnt + delete_image('outputs/img2img-grids')
+        del_cnt = del_cnt + delete_image('outputs/extras-images')
+        process_logs = process_logs + now.strftime("%Y-%m-%d %H:%M:%S") + "-> done clean all images..." + "total delete:{}\n".format(del_cnt)
     return process_logs
 
 def clean_all():
-    pass
+    return base_clean_action(3)
 
 def delete_image(input_path):
-    # /kaggle/working/stable-diffusion-webui/outputs/txt2img-images
-    # /kaggle/working/stable-diffusion-webui/outputs/txt2img-grids
-    # /kaggle/working/stable-diffusion-webui/outputs/img2img-grids
-    # /kaggle/working/stable-diffusion-webui/outputs/img2img-images
-    # /kaggle/working/stable-diffusion-webui/outputs/extras-images
+    sum = 0
     folder_path = os.path.join(ROOT_DIR, input_path)
     if not os.path.isdir(folder_path):
         print(f"Warning: {folder_path} is not a directory.")
@@ -42,21 +52,22 @@ def delete_image(input_path):
             file_path = os.path.join(folder_path, f)
             if os.path.isfile(file_path):
                 os.remove(file_path)
+                sum += 1
 
-
+    return sum
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
         txt_3 = gr.Textbox(value="", label="Logs", lines=3)
 
-        btn_textImage = gr.Button(value="Text To Image")
+        btn_textImage = gr.Button(value="Clean Text To Image Output")
         btn_textImage.click(clean_text_to_images, outputs=[txt_3])
 
-        btn_imageImage = gr.Button(value="Image To Image")
+        btn_imageImage = gr.Button(value="Clean Image To Image Output")
         btn_imageImage.click(clean_image_to_images, outputs=[txt_3])
 
-        btn_all = gr.Button(value="ALL")
+        btn_all = gr.Button(value="Clean ALL Output")
         btn_all.click(clean_all, outputs=[txt_3])
-        return [(ui_component, "sd_extension_clean_images", "sd_extension_clean_images")]
+        return [(ui_component, "Clean Output Images", "Clean Output Images")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
